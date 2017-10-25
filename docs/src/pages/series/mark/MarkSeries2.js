e8fce8eca3d9a22fb6f9b5fdd6d2fb3a981bb685
Michael Angerman <stormasm@gmail.com>
2017-10-25 10:22:07 -0700

    showcase button is up and running

--- docs/src/pages/series/mark/MarkSeries2.js
+++ docs/src/pages/series/mark/MarkSeries2.js
@@ -1,8 +1,5 @@
-// @flow weak
-
 import React from 'react';
 import PropTypes from 'prop-types';
-import { withStyles } from 'material-ui/styles';
 
 import {
   XYPlot,
@@ -10,52 +7,110 @@ import {
   YAxis,
   VerticalGridLines,
   HorizontalGridLines,
-  AreaSeries
+  MarkSeries,
+  MarkSeriesCanvas,
+  Hint
 } from 'react-vis';
 
-const styles = theme => ({
-  root: {
-    marginTop: theme.spacing.unit * 3,
-    width: '100%',
-    opacity: 1,
-    stroke: "blue",
-    fill: theme.palette.background.contentFrame
-  },
-  flex: {
-    flex: 1,
-  },
-  menuButton: {
-    marginLeft: -12,
-    marginRight: 20,
-  },
-});
-
-function MarkSeries2(props) {
-  const { classes } = props;
-  return (
-    <div className={classes.root}>
-      <XYPlot
-        width={300}
-        height={300}>
-        <VerticalGridLines />
-        <HorizontalGridLines />
-        <XAxis />
-        <YAxis />
-        <AreaSeries
-          className="area-series-example"
-          curve="curveNatural"
-          data={[
-            {x: 1, y: 10},
-            {x: 2, y: 5},
-            {x: 3, y: 15}
-          ]}/>
-      </XYPlot>
-    </div>
-  );
+class ShowcaseButton extends React.Component {
+  render() {
+    const {buttonContent, onClick} = this.props;
+    return (
+      <button
+        className="showcase-button"
+        onClick={onClick}>
+        {buttonContent}
+      </button>
+    );
+  }
 }
 
-MarkSeries2.propTypes = {
-  classes: PropTypes.object.isRequired,
+ShowcaseButton.PropTypes = {
+  buttonContent: PropTypes.string.isRequired,
+  onClick: PropTypes.func.isRequired
 };
 
-export default withStyles(styles)(MarkSeries2);
+function getRandomData() {
+  return (new Array(100)).fill(0).map(row => ({
+    x: Math.random() * 10,
+    y: Math.random() * 20,
+    size: Math.random() * 10,
+    color: Math.random() * 10,
+    opacity: Math.random() * 0.5 + 0.5
+  }));
+}
+const colorRanges = {
+  typeA: ['#59E4EC', '#0D676C'],
+  typeB: ['#EFC1E3', '#B52F93']
+};
+
+const randomData = getRandomData();
+const nextType = {
+  typeA: 'typeB',
+  typeB: 'typeA'
+};
+
+const nextModeContent = {
+  canvas: 'SWITCH TO SVG',
+  svg: 'SWITCH TO CANVAS'
+};
+
+const drawModes = ['canvas', 'svg'];
+
+export default class MarkSeries2 extends React.Component {
+  state = {
+    drawMode: 0,
+    data: randomData,
+    colorType: 'typeA',
+    value: false
+  }
+
+  render() {
+    const {drawMode, data, colorType} = this.state;
+    const markSeriesProps = {
+      animation: true,
+      className: 'mark-series-example',
+      sizeRange: [5, 15],
+      seriesId: 'my-example-scatterplot',
+      colorRange: colorRanges[colorType],
+      opacityType: 'literal',
+      data,
+      onNearestXY: value => this.setState({value})
+    };
+
+    const mode = drawModes[drawMode];
+    return (
+      <div className="canvas-wrapper">
+        <div className="canvas-example-controls">
+          <div>{`MODE: ${mode}`}</div>
+          <ShowcaseButton
+            onClick={() => this.setState({drawMode: (drawMode + 1) % 2})}
+            buttonContent={nextModeContent[mode]} />
+          <ShowcaseButton
+            onClick={() => this.setState({data: getRandomData()})}
+            buttonContent={'UPDATE DATA'} />
+          <ShowcaseButton
+            onClick={() => this.setState({colorType: nextType[colorType]})}
+            buttonContent={'UPDATE COLOR'} />
+        </div>
+        <XYPlot
+          onMouseLeave={() => this.setState({value: false})}
+          width={600}
+          height={300}>
+          <VerticalGridLines />
+          <HorizontalGridLines />
+          <XAxis />
+          <YAxis />
+          {mode === 'canvas' &&
+            <MarkSeriesCanvas {...markSeriesProps}/>}
+          {mode === 'svg' &&
+            <MarkSeries {...markSeriesProps}/>}
+          {this.state.value ?
+            <Hint value={this.state.value}/> :
+            null
+          }
+        </XYPlot>
+      </div>
+    );
+  }
+}
