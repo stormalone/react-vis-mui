28f09c38b13e613ae1fef770418210038e74792b
Michael Angerman <stormasm@gmail.com>
2017-10-25 10:09:03 -0700

    MarkSeries1 is good

--- docs/src/pages/series/mark/MarkSeries1.js
+++ docs/src/pages/series/mark/MarkSeries1.js
@@ -10,7 +10,7 @@ import {
   YAxis,
   VerticalGridLines,
   HorizontalGridLines,
-  AreaSeries
+  MarkSeries
 } from 'react-vis';
 
 const styles = theme => ({
@@ -41,13 +41,17 @@ function MarkSeries1(props) {
         <HorizontalGridLines />
         <XAxis />
         <YAxis />
-        <AreaSeries
-          className="area-series-example"
-          curve="curveNatural"
+        <MarkSeries
+          className="mark-series-example"
+          strokeWidth={2}
+          opacity="0.8"
+          sizeRange={[5, 15]}
           data={[
-            {x: 1, y: 10},
-            {x: 2, y: 5},
-            {x: 3, y: 15}
+            {x: 1, y: 10, size: 30},
+            {x: 1.7, y: 12, size: 10},
+            {x: 2, y: 5, size: 1},
+            {x: 3, y: 15, size: 12},
+            {x: 2.5, y: 7, size: 4}
           ]}/>
       </XYPlot>
     </div>
